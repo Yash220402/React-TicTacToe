6cbdd883aa6a69e1646f3f8d0cfe58b7feb42881
Yash220402 <yash220402@gmail.com>
2023-06-05 23:11:14 +0530

    implement taking turns

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -8,13 +8,22 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
+function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) { return; }
+ 
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
@@ -37,3 +46,5 @@ export default function Board() {
     </>
   );
 }
+
+export default Board;
